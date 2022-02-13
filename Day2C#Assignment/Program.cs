3313847b700973331dea05136f01c207a8d14f0f
singhsaurabhss <singhsaurabhss482@gmail.com>
2022-02-13 23:58:02 +0530

    Day2AssignmentC#
     write a program to take 5 student rollno, and their respective name and 5 subjects marks of each student
    using  array. After collecting every student details
     generate the progress of each student.
    
    roll no Name  mark1 mark2 mark3 mark4 mark5 total Grade
    
    pass percentage 60
    if the student is pass in all subject
     grade 90> 100 Excellent
    80>90 good
    70 > 80 fair
    60> 70  Need to improve

---
+++ Day2C#Assignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemoClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student student = new Student();
+
+            Student[] student = new Student[2];
+            for(int i = 0; i < 2; i++)
+            {
+                student[i] = new Student();
+                student[i].getStudentDetail();
+            }
+            Console.WriteLine("Roll no  Name \t Mark1 \t Mark2 \t Mark3 \t Mark4 \t Mark5 \t Total \t Grade \n");
+            for (int i = 0; i < 2; i++)
+            {
+                student[i].printStudentDetail();
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day2C#Assignment/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsDemoClass
+{
+    class Student
+    {
+        int sid, mark1, mark2, mark3, mark4, mark5, total;
+        double percentage;
+        string name, grade;
+        public void getStudentDetail()
+        {
+            Console.WriteLine("Enter the student id,Name, Mark1, Mark2, Mark3, Mark4, Mark5 : ");
+            sid = Convert.ToInt32(Console.ReadLine());
+            name = Console.ReadLine();
+            mark1 = Convert.ToInt32(Console.ReadLine());
+            mark2 = Convert.ToInt32(Console.ReadLine());
+            mark3 = Convert.ToInt32(Console.ReadLine());
+            mark4 = Convert.ToInt32(Console.ReadLine());
+            mark5 = Convert.ToInt32(Console.ReadLine());
+            
+        }
+        public void printStudentDetail()
+        {
+            total = mark1 + mark2 + mark3 + mark4 + mark5;
+            /*Console.WriteLine("Student Id " + sid);
+            Console.WriteLine("Student Name " + name);
+            Console.WriteLine("Mark 1 " + mark1);
+            Console.WriteLine("Mark 2 : " + mark2);
+            Console.WriteLine("Mark 3 " + mark3);
+            Console.WriteLine("Mark 4 " + mark4);
+            Console.WriteLine("Mark 5 : " + mark5);
+            Console.WriteLine("Total = " + total);*/
+
+            percentage = total / 5;
+
+            if (percentage < 100 && percentage > 90)
+                grade = "Excellent";
+            else if (percentage < 90 && percentage > 80)
+                grade = "Good";
+            else if (percentage < 80 && percentage > 70)
+                grade = "Fair";
+            else if (percentage < 70 && percentage > 60)
+                grade = "Need to Improve";
+            else
+                grade = "Fail";
+
+            // displaying information
+                Console.WriteLine($"{this.sid} \t {this.name} \t {this.mark1} \t {this.mark2} \t {this.mark3} \t {this.mark4} \t {this.mark5} \t {this.total} \t {this.grade} \n");
+            
+        }
+    }
+}
